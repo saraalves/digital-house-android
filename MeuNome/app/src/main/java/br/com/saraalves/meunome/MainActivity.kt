043e913d0f3974de4a6ea77230bbdaf54a5cfbed
Sara Alves <saraalves14@hotmail.com>
2020-10-18 23:17:02 -0300

    meu nome aula 16

--- MeuNome/app/src/main/java/br/com/saraalves/meunome/MainActivity.kt
+++ MeuNome/app/src/main/java/br/com/saraalves/meunome/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.saraalves.meunome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
