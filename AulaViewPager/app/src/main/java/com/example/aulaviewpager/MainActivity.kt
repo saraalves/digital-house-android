e6a664b97a818a8f494c5a3a10336e2382857ee6
Sara Alves <saraalves14@hotmail.com>
2020-10-23 20:04:22 -0300

    aula 24 - configuração do adapter

--- AulaViewPager/app/src/main/java/com/example/aulaviewpager/MainActivity.kt
+++ AulaViewPager/app/src/main/java/com/example/aulaviewpager/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.aulaviewpager
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pager = findViewById<ViewPager>(R.id.viewPager)
+        val tab = findViewById<TabLayout>(R.id.tabLayout)
+
+        // Faz com que o tab use o viewPager
+        tab.setupWithViewPager(pager)
+
+        val fragments = listOf(
+            PrimeiroFragment(), PrimeiroFragment(), PrimeiroFragment()
+        )
+
+        val titulos = listOf(
+           "Home", "Biblioteca", "Favoritos"
+        )
+
+        pager.adapter = ViewPagerAdapter(fragments, titulos, supportFragmentManager)
     }
 }
--- AulaViewPager/app/src/main/java/com/example/aulaviewpager/ViewPagerAdapter.kt
+++ AulaViewPager/app/src/main/java/com/example/aulaviewpager/ViewPagerAdapter.kt
@@ -4,12 +4,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    override fun getItem(position: Int): Fragment {
-        TODO("Not yet implemented")
-    }
+class ViewPagerAdapter(private val fragments: List<Fragment>,
+                       private val titulos: List<String>,
+                       manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    // retorna o fragment correspondente a posição
+    override fun getItem(position: Int)  = fragments[position]
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
-    }
+    // quantidade de tabs
+    override fun getCount() = fragments.size
+
+    // retorna o nome da tab
+    override fun getPageTitle(position: Int) = titulos[position]
 }
