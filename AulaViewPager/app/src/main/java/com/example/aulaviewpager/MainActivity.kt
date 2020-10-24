152d36aa555227bb025cb1fd8852cb3c19960532
Sara Alves <saraalves14@hotmail.com>
2020-10-23 22:16:39 -0300

    configurando os números de notificações nas tabs

--- AulaViewPager/app/src/main/java/com/example/aulaviewpager/MainActivity.kt
+++ AulaViewPager/app/src/main/java/com/example/aulaviewpager/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.aulaviewpager
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
@@ -17,24 +16,54 @@ class MainActivity : AppCompatActivity() {
         // Faz com que o tab use o viewPager
         tab.setupWithViewPager(pager)
 
-        val fragments = listOf(
-            PrimeiroFragment(), PrimeiroFragment(), PrimeiroFragment()
-        )
+//        val fragments = listOf(
+//            PrimeiroFragment(), PrimeiroFragment(), PrimeiroFragment()
+//        )
 
         val titulos = listOf(
            getString(R.string.home), getString(R.string.biblioteca), getString(R.string.favoritos)
         )
 
+        val fragments = listOf(
+            PrimeiroFragment.newInstance("Sara", true),
+            PrimeiroFragment.newInstance("Julia", false),
+            PrimeiroFragment.newInstance("Roberto", true)
+        )
+
         pager.adapter = ViewPagerAdapter(fragments, titulos, supportFragmentManager)
 
+        // adiciona icones na tab
         tab.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_home_24)
         tab.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_menu_book_24)
         tab.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_star_24)
 
-        tab.getTabAt(0)!!.orCreateBadge.apply {
+        // adiciona badge na segunda tab
+        tab.getTabAt(1)!!.orCreateBadge.apply {
             number = 10
             maxCharacterCount = 2
             isVisible = true
         }
+
+
+
+        // a ideia é fazer com que ao clicar na segunda tab, que é a que tem notificação ,
+        // essa notificação seja ocultado, pois o usuário j[a leu as novidades dela
+        tab.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(selected: TabLayout.Tab?) {
+                if(selected!!.position == 1){
+
+                    tab.getTabAt(1)!!.badge?.isVisible = false
+
+                }
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 }
