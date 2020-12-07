8c01fdf822b5138fbd048c3723ec5d3db414847a
Sara Alves <saraalves14@hotmail.com>
2020-12-07 20:41:02 -0300

    exercicio em aula sobre shared preferences

--- LoginWithSharedPreferences/app/src/main/java/com/example/loginwithsharedpreferences/SplashActivity.kt
+++ LoginWithSharedPreferences/app/src/main/java/com/example/loginwithsharedpreferences/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.example.loginwithsharedpreferences
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val goToLogin = findViewById<TextView>(R.id.txtGotoLogin)
+        goToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
